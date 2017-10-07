ed83c34e1004dc8c8371eac5673dbf4b0381f7cd
Timothy Odom <tim.odom@thinkahead.com>
2017-10-07 13:09:52 -0500

    asd

--- site/profile/manifests/master.pp
+++ site/profile/manifests/master.pp
@@ -1,6 +1,5 @@
 # Class: profile::master
 #
-#
 class profile::master {
   # class { '::report2slack':
   #   webhook       => 'https://hooks.slack.com/services/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',
