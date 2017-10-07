a6fde6a6edcc687646200e200bf335881b2addca
Timothy Odom <tim.odom@thinkahead.com>
2017-10-06 20:26:20 -0500

    sdfds

--- site/profile/manifests/master.pp
+++ site/profile/manifests/master.pp
@@ -9,7 +9,6 @@ class profile::master {
   # }
 
   class { '::report2snow':
-    #url => 'https://aheaddemo2.service-now.com/api/now/table/change_request?sysparm_fields=',
     #url => 'https://aheaddemo2.service-now.com/api/now/table/change_request?sysparm_input_display_value=true',
     url                 => 'https://dev31247.service-now.com/api/now/table/incident',
     servicenow_username => 'admin',
